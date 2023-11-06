e518898945c14f488f4e92d1f1fa1444b814da29
Jeeyeon-Lee <jeeyeon118@gmail.com>
2023-11-06 17:34:19 +0900

    20231106

---
+++ vscode-express/myBook/public/javascripts/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js';
+const firebaseConfig = {
+  apiKey: 'AIzaSyCltWlMRHeCS61FnAfLs3B-hsgaK_o5Nl0',
+  authDomain: 'goodee-project-b2af0.firebaseapp.com',
+  projectId: 'goodee-project-b2af0',
+  storageBucket: 'goodee-project-b2af0.appspot.com',
+  messagingSenderId: '457035965872',
+  appId: '1:457035965872:web:dc2ccf6a403dd450d0e275',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
